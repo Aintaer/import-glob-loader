3088c1fb73dcf9e9ec6a2315d261b677e4cc1689
Alex Lee <aintaer@aintaer.com>
2015-07-11 17:00:32 -0400

    Vertical expansion mode
    Instead of horizontal expansions with commas, which is supported only in
    Sass. Vertical expansions (duplicate line with different content of
    quoted string) work for a wider number of cases.

--- src/index.js
+++ src/index.js
@@ -7,21 +7,29 @@ export default function importGlob(source) {
 	options.nodir = typeof options.nodir !== 'undefined' ? options.nodir : true;
 	options.cwd = this.context;
 
-	let { test = "import", delimiter = ', ' } = options;
+	let { test = "import", delimiter = '\n' } = options;
 	const qualifier = new RegExp(`^.*\\b${test}\\b(.*)$`, 'gm');
 
-	function expandGlob(match, quote, content) {
-		if (!glob.hasMagic(content)) return match;
+	function expandGlob(result) {
+		if (!result) return;
+		const [match, quote, content] = result;
+		const offset = result.index;
+		const line = result.input;
+
+		if (!glob.hasMagic(content)) return;
+
+		let pre = line.slice(0, offset),
+			post = line.slice(offset + match.length);
 
 		return glob.sync(content, options)
-		.map(filename => `${quote}${filename}${quote}`)
+		.map(filename => `${pre}${quote}${filename}${quote}${post}`)
 		.join(delimiter);
 	}
 
-	const quotedString = /(['"])(.*?)\1/g;
+	const quotedString = /(['"])(.*?)\1/;
 	function expandLine(line, payload) {
-		if (!payload) return line;
-		return line.replace(quotedString, expandGlob);
+		if (!(payload && payload.trim())) return line;
+		return expandGlob(quotedString.exec(line)) || line;
 	}
 
 	return source.replace(qualifier, expandLine);
