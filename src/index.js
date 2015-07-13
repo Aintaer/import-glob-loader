12bbd3f3aadb70be52da4212fe74330495278c6b
Alex Lee <aintaer@gmail.com>
2015-07-13 14:31:24 -0400

    Cacheable loader
    Sets loader as cacheable, with context dependencies on all globbed
    subdirectories.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
+/* vim: set ts=4 sw=4 noet: */
 import loaderUtils from 'loader-utils';
 import glob from 'glob';
 
+const quotedString = /(['"])(.*?)\1/;
+const trailingSlash = /\/$/;
+
 export default function importGlob(source) {
+	this.cacheable();
 	const options = loaderUtils.parseQuery(this.query);
-	// Default nodir to true
-	options.nodir = typeof options.nodir !== 'undefined' ? options.nodir : true;
-	options.cwd = this.context;
+	options.sync = true;
 
 	let { test = "import", delimiter = '\n' } = options;
 	const qualifier = new RegExp(`^.*\\b${test}\\b(.*)$`, 'gm');
@@ -21,16 +24,30 @@ export default function importGlob(source) {
 		let pre = line.slice(0, offset),
 			post = line.slice(offset + match.length);
 
-		return glob.sync(content, options)
+		options.cwd = this.context;
+		const dirGlob = new glob.Glob(
+			trailingSlash.test(content) ?  content : `${content}/`, options);
+
+		dirGlob.found
+		.forEach(directory => this.addContextDependency(directory));
+
+		const fileOptions = Object.create(options);
+		if (!options.hasOwnProperty('nodir')) {
+			fileOptions.nodir = true;
+		}
+		fileOptions.cwd = this.context;
+		fileOptions.cache = dirGlob.cache;
+		fileOptions.statCache = dirGlob.statCache;
+
+		return glob.sync(content, fileOptions)
 		.map(filename => `${pre}${quote}${filename}${quote}${post}`)
 		.join(delimiter);
 	}
 
-	const quotedString = /(['"])(.*?)\1/;
 	function expandLine(line, payload) {
 		if (!(payload && payload.trim())) return line;
-		return expandGlob(quotedString.exec(line)) || line;
+		return expandGlob.call(this, quotedString.exec(line)) || line;
 	}
 
-	return source.replace(qualifier, expandLine);
+	return source.replace(qualifier, expandLine.bind(this));
 }
